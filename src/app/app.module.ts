78a42ef960052d529b8708d042f95803d49a7832
eccentricnode <shival.bigman@gmail.com>
2020-02-25 16:54:46 -0700

    incremental checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomePageModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    HomePageModule,
+    AppRoutingModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
+    Insomnia,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,98 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  percent: number = 0;
+  radius: number = 100;
+  fullTime: any = '00:01:30';
 
+  timer: any = false;
+  progress: any = 0;
+  minutes: number = 1;
+  seconds: any = 30;
+
+  elapsed: any = {
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  }
+
+  overallTimer: any = false;
+
+  constructor(private insomnia: Insomnia) {
+
+  }
+
+  startTime() {
+
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+    if(!this.overallTimer) {
+      this.progressTimer();
+      this.insomnia.keepAwake();
+    }
+
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+
+    let timeSplit = this.fullTime.split(':');
+    this.minutes = timeSplit[1];
+    this.seconds = timeSplit[2];
+
+    let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+
+    this.timer = setInterval(() => {
+
+      if (this.percent == this.radius) {
+        clearInterval(this.timer);
+      }
+
+      this.percent = Math.floor((this.progress / totalSeconds) * 100);
+      this.progress++;
+    }, 1000);
+  }
+
+  progressTimer() {
+    let countDownDate = new Date();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = now - countDownDate.getTime();
+
+      this.elapsed.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.hours = this.pad(this.elapsed.hours, 2);
+      this.elapsed.minutes = this.pad(this.elapsed.minutes, 2);
+      this.elapsed.seconds = this.pad(this.elapsed.seconds, 2);
+    }, 1000);
+  }
+
+  pad(number, size) {
+    let s = number+'';
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
+
+  stopTime() {
+    clearInterval(this.timer);
+    clearInterval(this.overallTimer);
+    this.overallTimer = false;
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+    this.elapsed = {
+      hours: '00',
+      minutes: '00',
+      seconds: '00'
+  };
+
+  this.insomnia.allowSleepAgain();
+  }
+
+  updateMyDate($event) {
+    console.log($event.split(':'));
+  }
 }
